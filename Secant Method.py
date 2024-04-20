1fb111511c521ac71b09b693e98e576b25f8442f
Dalmanski <jayraldjohndalman@gmail.com>
2024-04-20 22:47:53 +0800

    Change wrong yt link + new equations example

--- Secant Method.py
+++ Secant Method.py
@@ -3,10 +3,11 @@ import sympy as sp
 x = sp.symbols("x")
 
 # Example 1: x^2 - x - 1 = 0, x_a = 3, x_b = 2
-# The correct answer is 1.618, proof? On Yt video: https://www.youtube.com/watch?v=lN1JXolkKCM&t=121s
-
+# The correct answer is 1.618, proof? On Yt video: https://www.youtube.com/watch?v=AN6YxAoUqLM&t=435s
 # Example 2: x^4 - x - 10, x_a = 1, x_b = 2
-# The correct answer is 1.8556, proof? On Yt video: https://www.youtube.com/watch?v=3g19OMfCpCA
+# The correct answer is 1.856, proof? On Yt video: https://www.youtube.com/watch?v=3g19OMfCpCA
+# Example 3: x^2 - 2, x_a = 1, x_b= 2
+# The correct answer is 1.414, proof? On Yt video: https://www.youtube.com/watch?v=Zz8AYQ8c5-U&t=251s
 
 equation_str = input("Enter your equation: ")
 equation_str = equation_str.replace("^", "**")
@@ -22,6 +23,8 @@ x_b = float(input("Enter your x_b: "))
 
 prev_x_new = 0
 i = 0
+
+print()
 
 while True:
 
